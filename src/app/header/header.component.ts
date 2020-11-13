b307978d5b696bad315769d8f53684ff0336a353
Waylon Carrier Nunes <wayloncn@gmail.com>
2020-11-13 16:03:07 -0500

    created and layed out the header component for mobile

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  public logo = {
+    imagePath: '../../assets/images/logo.svg',
+    altText: 'A logo that says Rock Paper Scissors.',
+  };
+  public currentScore = 12;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
